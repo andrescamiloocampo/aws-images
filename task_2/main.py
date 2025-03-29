0e930212902ca36b463117d183d8f2023cceeaff
sala104 <sala104@poliacademico.edu.loc>
2025-03-29 11:14:38 -0500

    Task 2

---
+++ task_2/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import boto3
+
+app = Flask(__name__)
+
+s3_client = boto3.client('s3')
+rekognition_client = boto3.client('rekognition')
+
+@app.route('/analyze', methods=['GET'])
+def analyze_image():
+    image_name = request.args.get('image_name')
+    bucket_name = request.args.get('bucket_name')
+
+    if not image_name or not bucket_name:
+        return jsonify({'error': 'Se deben proporcionar el nombre de la imagen y el bucket'}), 400
+
+    response = rekognition_client.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': bucket_name,
+                'Name': image_name
+            }
+        },
+        MaxLabels=10,
+        MinConfidence=75
+    )
+        
+    labels = response['Labels']
+    return jsonify([(label['Name'], label['Confidence']) for label in labels])
+
+if __name__ == '__main__':
+    app.run(debug=True)
