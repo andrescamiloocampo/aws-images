a916eeb00728bd96609f13bcfe9e196340359c88
Andres Camilo Ocampo Ramirez <andres_ocampo82211@elpoli.edu.co>
2025-03-31 09:42:57 -0500

    Change host

--- task_2/main.py
+++ task_2/main.py
@@ -29,4 +29,4 @@ def analyze_image():
     return jsonify([(label['Name'], label['Confidence']) for label in labels])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
